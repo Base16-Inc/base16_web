3684b986ffa7bd143125efdb194cf7ac3e25d08a
Andrew Kim <jeesoo2002@gmail.com>
2023-11-13 11:45:11 +0900

    added google html tag to head

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <title>Base 16</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Building Things" />
+      <meta name="google-site-verification" content="9mvXPEAexE5GARJgHr5d7duLnJl2khe9AfwFr9KQLOA" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
