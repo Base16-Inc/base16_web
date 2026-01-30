cf249e8c46af0421ca14c4db45010d78d09d8acb
Daniel Kim <daniel.sanghkim@gmail.com>
2026-01-30 14:55:12 -0800

    remove linkedin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link';
 import React from 'react';
-import linkedin_logo from '../data/linkedin_logo.png'
 import logo from '../data/b16_logo_clear.png'
 
 const Footer: React.FC = () => {
@@ -23,17 +22,6 @@ const Footer: React.FC = () => {
           <a className='mr-3' href='mailto:support@base-16.com'>support@base-16.com</a>
         </div>
         <div className='flex items-center'>
-          <Link href='https://www.linkedin.com/company/base16-inc/about/'>
-            <Image
-              src={linkedin_logo}
-              width={24}
-              height={24}
-              alt='linkedin logo'
-              style={{
-                marginRight: '1em'
-              }}
-            />
-          </Link>
           <p className='mr-2 text-sm'>© 2026 Base16 Inc. All Rights Reserved</p>
         </div>
       </div>
